060d5cb51f479f5ebb7331b939b20d317d65fa95
dron <Dimitrij.Ron@sn-invent.de>
2026-02-10 12:10:04 +0100

    fix

--- sn-webshop-client/src/app/feature/product/page/product/product.component.ts
+++ sn-webshop-client/src/app/feature/product/page/product/product.component.ts
@@ -1,24 +1,30 @@
 import { Component, ChangeDetectionStrategy, inject, input } from '@angular/core';
-import { first, switchMap } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
 import { AsyncPipe, JsonPipe } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { ProductInfoComponent } from '@product/component/product-info/product-info.component';
 import { ProductBasketOptionsComponent } from '@product/component/product-basket-options/product-basket-options.component';
-import { ProductService } from '@shared/service/product/product.service';
 import { CheckoutService } from '@shared/service/checkout/checkout.service';
 import { DetailedProduct } from '@shared/model/product/detailed-product';
+import { toObservable } from '@angular/core/rxjs-interop';
 
 @Component({
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [MatCardModule, ProductInfoComponent, ProductBasketOptionsComponent, JsonPipe],
+  imports: [MatCardModule, ProductInfoComponent, ProductBasketOptionsComponent, AsyncPipe],
 })
 export class ProductComponent {
   private readonly checkoutService = inject(CheckoutService);
 
   readonly detailedProduct = input.required<DetailedProduct>();
+
+  readonly quantity$ = toObservable(this.detailedProduct).pipe(
+    switchMap((product) => {
+      return this.checkoutService.getQuantity(product.id);
+    }),
+  );
 
   addToBasket(): void {
     const detailedProduct = this.detailedProduct();
