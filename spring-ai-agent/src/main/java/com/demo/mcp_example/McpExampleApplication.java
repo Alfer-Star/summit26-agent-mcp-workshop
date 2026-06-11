d65efadb4d1d14ab85919da5e6397568e1077901
jkirchner <jochen.kirchner@sn-invent.de>
2026-06-11 10:53:58 +0200

    Neue erzeugtes Spring Boozt Projekt

---
+++ spring-ai-agent/src/main/java/com/demo/mcp_example/McpExampleApplication.java
@@ -0,0 +1,13 @@
+package com.demo.mcp_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpExampleApplication.class, args);
+	}
+
+}
